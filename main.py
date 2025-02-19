1919110e81c9068d465c8bfc8c563f3b9d341f86
Phatjira Rungsakullikit <p.rungsakullikit1@hm.edu>
2025-02-19 12:04:45 +0100

    Final Code

--- main.py
+++ main.py
@@ -35,13 +35,11 @@ import pandas
 # Dictionary comprehension with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_alphabet_dict = {row.letter:row.code for index, row in data.iterrows()}
 print(phonetic_alphabet_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
 def generate_phonetic():
     word = input("Enter a word: ").upper()
     try:
